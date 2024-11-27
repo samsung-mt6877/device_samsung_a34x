dfe0d8fbe4a591ad39df30386b0f23b388da2949
semfault <semfault@proton.me>
2024-11-28 03:37:23 +0800

    a34x: Inital minimal rootdir
    Change-Id: I9e5970d8004cc7b76d5008aa094f8438c416c1dd
    Signed-off-by: semfault <semfault@proton.me>

--- device.mk
+++ device.mk
@@ -10,6 +10,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.insmod.sh
+
+PRODUCT_PACKAGES += \
+    fstab.mt6877 \
+    init.ago.rc \
+    init.audio.samsung.rc \
+    init.cgroup.rc \
+    init.connectivity.common.rc \
+    init.connectivity.rc \
+    init.modem.rc \
+    init.mt6877.rc \
+    init.mt6877.usb.rc \
+    init.project.rc \
+    init.sec.rc \
+    init_conninfra.rc \
+    multi_init.rc \
+    init.recovery.mt6877.rc \
+    init.recovery.samsung.rc
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 33
 
