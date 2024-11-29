c03c2808af234d7ded17935152d1aa0cacaa4914
bengris32@protonmail.ch <bengris32@protonmail.ch>
2024-11-29 14:42:01 +0800

    a34x: Configure dynamic partitions
    [semfault: also define board super size]
    
    Change-Id: I080cb8f7bc29cb0cd6dcb153d22dfbc5c1eb5827
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Co-authored-by: ant <ant@national.shitposting.agency>
    Signed-off-by: semfault <semfault@proton.me>

--- device.mk
+++ device.mk
@@ -7,6 +7,11 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
